67c8336d141e8074d88922d1a080247bdd235b05
p2733814 <p2733814@GWHXX631WIN010W>
2023-10-25 15:34:39 +0100

    Date: 25/10/2023

--- Application_Clean/src/myScene.cpp
+++ Application_Clean/src/myScene.cpp
@@ -16,27 +16,48 @@ Myscene::~Myscene()
 void Myscene::makeVAO()
 {
 	glCreateBuffers(1, &VBO);
-	glNamedBufferStorage(VBO, sizeof(float) * 36, vertexData,  GL_DYNAMIC_STORAGE_BIT); // 9 is number of floats in vertexData array
+	glNamedBufferStorage(VBO, sizeof(float) *  vertexData.size(),vertexData.data(), GL_DYNAMIC_STORAGE_BIT); // 9 is number of floats in vertexData array
+
+	glCreateBuffers(1, &EBO);
+	glNamedBufferStorage(EBO, sizeof(unsigned int) * cubeIndices.size(), cubeIndices.data(),GL_DYNAMIC_STORAGE_BIT);
 
 	glCreateVertexArrays(1, &VAO);
-	glVertexArrayVertexBuffer(VAO, 0, VBO, 0, sizeof(float) * 6);
+	glVertexArrayVertexBuffer(VAO, 0, VBO, 0,sizeof(float) * 3);
+	glVertexArrayElementBuffer(VAO, EBO);
 
 	glEnableVertexArrayAttrib(VAO, 0);
-	glEnableVertexArrayAttrib(VAO, 1);
 
 	glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
-	glVertexArrayAttribFormat(VAO, 1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float));
 
 	glVertexArrayAttribBinding(VAO, 0, 0);
-	glVertexArrayAttribBinding(VAO, 1, 0);
+	
 
 }
 
 void Myscene::render()
 {
+	m_model = glm::mat4(1.0f);
+	m_projection = m_camera->getProjectionMatrix();
+	m_view = m_camera->getViewMatrix();
 	m_myShader->use();
+
+	m_myShader->setMat4("View", m_view);
+	m_myShader->setMat4("Projection", m_projection);
+	m_myShader->setMat4("Model", m_model);
+
+
 	glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawElements(GL_TRIANGLES, vertexData.size(), GL_UNSIGNED_INT, 0);
+
+	m_model = glm::translate(m_model, glm::vec3(5.0, 0.0, 0.0));
+	m_myShader->setMat4("Model", m_model);
+
+	glDrawElements(GL_TRIANGLES, vertexData.size(), GL_UNSIGNED_INT, 0);
+
+	m_model = glm::translate(m_model, glm::vec3(7.0, 0.0, 0.0));
+	m_myShader->setMat4("Model", m_model);
+
+	glDrawElements(GL_TRIANGLES, vertexData.size(), GL_UNSIGNED_INT, 0);
 }
 
 void Myscene::update(float dt) {
